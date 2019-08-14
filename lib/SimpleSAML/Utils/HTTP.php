f49c6024e7cb653bfc32c042e48b26e7cea8468f
Filip Hanik <fhanik@pivotal.io>
2019-08-14 12:13:35 -0700

    [Cloud Foundry Customization]
    If the server is behind a proxy, correctly set the HTTPS port

--- lib/SimpleSAML/Utils/HTTP.php
+++ lib/SimpleSAML/Utils/HTTP.php
@@ -86,6 +86,13 @@ class HTTP
      */
     public static function getServerHTTPS()
     {
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '80') {
+               $_SERVER['SERVER_PORT'] = 443;
+            }
+            $_SERVER['HTTPS'] = 'true';
+        }
+
         if (!array_key_exists('HTTPS', $_SERVER)) {
             // not an https-request
             return false;
